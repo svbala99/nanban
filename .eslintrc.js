38c10bcab7b0d33fe719de7c4c22c7088fe5d02b
Balamurugan V <balamurugan@divum.in>
2023-08-09 15:48:54 +0530

    build: commitlint, eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,64 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        'react-native/react-native': true,
+        jest: true,
+    },
+    settings: {
+        react: { version: 'detect' },
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard-with-typescript',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'airbnb',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:prettier/recommended',
+        'prettier',
+    ],
+
+    parserOptions: {
+        ecmafeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        project: ['./tsconfig.json', './packages/*/tsconfig.json'],
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'react-native', 'prettier'],
+    overrides: [
+        {
+            // feel free to replace with your preferred file pattern - eg. 'src/**/*Slice.ts'
+            files: ['**/slice.ts'],
+            // avoid state param assignment
+            rules: { 'no-param-reassign': ['error', { props: false }] },
+        },
+    ],
+    rules: {
+        'react/jsx-filename-extension': [
+            'error',
+            { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+        ],
+        'no-use-before-define': [
+            'error',
+            { functions: true, classes: true, variables: true },
+        ],
+        'react-native/no-unused-styles': 'error',
+        'react-native/split-platform-components': 'error',
+        'react-native/no-inline-styles': 'error',
+        'react-native/no-color-literals': 'error',
+        'react-native/no-raw-text': 'error',
+        'react-native/no-single-element-style-arrays': 'error',
+        'react/jsx-props-no-spreading': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/restrict-plus-operands': 'off',
+    },
 };
--- App.tsx
+++ App.tsx
@@ -1,27 +1,22 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 import React from 'react';
 import Config from 'react-native-config';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
 function App(): JSX.Element {
-
-  return (
-    <SafeAreaView style={[ styles.container]}>
-      <Text>{Config?.ENV}</Text>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>{Config?.ENV}</Text>
+        </SafeAreaView>
+    );
 }
 
 export default App;
